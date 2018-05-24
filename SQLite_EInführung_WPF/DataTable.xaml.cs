77427df24d7dd5e43e830a0fd97b1df2eb3ee3ad
Sebastian Rogacki <fiae.srsr@gmail.com>
2018-05-24 16:02:42 +0200

    Init commit

---
+++ SQLite_EInführung_WPF/DataTable.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+
+namespace SQLite_EInführung_WPF
+{
+    /// <summary>
+    /// Interaktionslogik für DataTable.xaml
+    /// </summary>
+    public partial class DataTable : Window
+    {
+        public DataTable()
+        {
+            InitializeComponent();
+
+            SQLiteConnection dbConnection = new SQLiteConnection("Data Source = SQLiteEinführung.sqlite; Version = 3; Password = MyPass");
+            dbConnection.Open();
+
+            string chooseSql = "SELECT * FROM personen";
+            SQLiteCommand ChooseCommand = new SQLiteCommand(chooseSql, dbConnection);
+            Data_Table_DataGrid.DataContext = ChooseCommand;
+
+            SQLiteDataReader reader = ChooseCommand.ExecuteReader();
+        }
+
+    }
+}
